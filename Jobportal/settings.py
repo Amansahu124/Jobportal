7e7938de124c323231a837d81260e9a14ae867fa
Aman <amansahu32567@gmail.com>
2025-12-06 20:56:21 +0530

    Updated ALLOWED_HOSTS to include Render domain

--- Jobportal/settings.py
+++ Jobportal/settings.py
@@ -85,14 +85,11 @@ WSGI_APPLICATION = 'Jobportal.wsgi.application'
 # Updated for environment variables (local and Render)
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.getenv("DB_NAME", "jobportal"),
-        'USER': os.getenv("DB_USER", "root"),
-        'PASSWORD': os.getenv("DB_PASSWORD", "root"),
-        'HOST': os.getenv("DB_HOST", "localhost"),
-        'PORT': os.getenv("DB_PORT", "3306"),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
 
 
 # Password validation
